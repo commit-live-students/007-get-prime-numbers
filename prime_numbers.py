744ca04c3aba13d9ad4e8fac01a0803cd2dd6f6b
seekingpeace <seekingpeace@users.noreply.github.com>
2017-06-02 06:09:25 +0000

    Done

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,14 +1,14 @@
 def get_prime_numbers(n):
-    isPrime=[[True]] * n
+    isPrime=[[True]] * (n+1)
     isPrime[0]=isPrime[1]=0
     ret=[]
-    for i in range(2,n):
+    for i in range(2,n+1):
         if(isPrime[i]):
             ret.append(i)
-            for j in range(i*2,n,i):
+            for j in range(i*2,n+1,i):
                 isPrime[j]=False
     return ret
 
-print(get_prime_numbers(10000))
+print(get_prime_numbers(11))
 
 #implemented using sieve of Eratosthenes
