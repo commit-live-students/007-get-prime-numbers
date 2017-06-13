d9ab5ffe26fa864390b5c63d44f95284108e613d
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-13 18:04:40 +0000

    Done

---
+++ prime_numbers.py
@@ -0,0 +1,18 @@
+def get_prime_numbers(inputNum):
+    num_list = []
+    flag = 0
+    if(inputNum > 1):
+        for i in range (2,inputNum+1):
+            flag = 0
+            for j in range (2,i):
+                if(i%j == 0):
+                    flag = 1
+                    break
+            if flag == 0:
+                num_list.append(i)
+                print num_list
+        return num_list
+    else:
+        print "Enter number greater than 1"
+
+print get_prime_numbers(10)
