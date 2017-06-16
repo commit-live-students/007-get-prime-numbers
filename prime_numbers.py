e7d8369e63afae291ba6fc456a5f9895dea1e01e
adinathauti <adinathauti@gmail.com>
2017-06-16 07:00:55 +0000

    Done

--- prime_numbers.py
+++ prime_numbers.py
@@ -6,10 +6,11 @@ def get_prime_numbers(n):
     for i in range(2,n):
         for j in range(2,i):
             if (i%j == 0 and i!=j):
-                ##print(i,"isn't prime")
+
                 is_prime = False
         if(is_prime==True):
             ls.append(i)
+
         is_prime = True
     return ls
 
