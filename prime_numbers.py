69c0eef3a4142490fc0319c061fdd5020168cd0f
nixen420 <nikhil.singh4008@gmail.com>
2017-06-26 11:10:08 +0000

    Done

---
+++ prime_numbers.py
@@ -0,0 +1,14 @@
+def get_prime_numbers(n):
+    prime=[]
+    if n > 1:
+        for i in range(2,n):
+            for j in range(2,i):
+                if (i%j==0):
+                        break
+            else:
+                prime.append(i)
+    else:
+        if n<=1:
+            print "Number is less than one"
+
+    return prime
